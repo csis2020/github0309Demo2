a6328a0fe16a09660a15a420015b94420c20d901
csis2020 <73615176+csis2020@users.noreply.github.com>
2021-03-09 14:09:23 -0800

    Update MainActivity.java
    commit remote change

--- app/src/main/java/com/example/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Github0309 Test");
+        //Yes. Success to push to remote repository!!! confirm!!!
+        
     }
 }
